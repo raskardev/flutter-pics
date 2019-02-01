d475400eaa92801047355df80f5bc8efbf1ff7d4
acopero <acopero@soamee.com>
2019-02-01 21:42:01 +0100

    Entire Pics application

---
+++ lib/src/app.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter += 1;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('Lets see images!!'),
+        ),
+      ),
+    );
+  }
+}
+
+// Must define a 'build' method that returns the widgets that *this* widget will show
---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> images;
+
+  ImageList(this.images);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (BuildContext context, int index) {
+        return buildImage(images[index]);
+      },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.grey,
+      )),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+              child: Image.network(image.url),
+              padding: EdgeInsets.only(
+                bottom: 15.0,
+              )),
+          Text(image.title),
+        ],
+      ),
+    );
+  }
+}
